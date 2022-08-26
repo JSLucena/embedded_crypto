5242e27ec92c530f9687b6f08234ea0552d79571
joaquim <joca.lucena@gmail.com>
2022-08-26 14:32:30 -0300

    first commit

---
+++ M0/Core/Inc/HIGHT.h
@@ -0,0 +1,23 @@
+/* HIGHT.h
+*
+ * Author: Vinicius Borba da Rocha
+ * Created: 13/08/2021
+ *
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#include "CTRMode.h"
+
+typedef struct
+{
+	uint8_t whiteningKeys[8];
+	uint8_t subkeys[128];
+} HightContext;
+
+void HIGHT_init(HightContext* context, uint8_t* key);
+void HIGHT_encrypt(HightContext* context, uint8_t* block, uint8_t* out);
+
+void HIGHT_main(CTRCounter* ctrNonce, int key_size);
